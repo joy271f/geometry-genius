55461ea7c8debac9e29bc59786123a1191141b75
joychandradas <joychandradas007@gmail.com>
2023-02-18 06:17:30 +0600

    Button is workes now

--- js/app.js
+++ js/app.js
@@ -1,27 +1,74 @@
 let serial = 0;
-document.getElementById('btn-circle').addEventListener('click', function(){
-    const circleInputOne = getInputValueOne('circle-input-one');
-    const circleInputTwo = getInputValueOne('circle-input-two');
+document.getElementById("btn-circle").addEventListener("click", function () {
+  const circleInputOne = getInputValueOne("circle-input-one");
+  const circleInputTwo = getInputValueOne("circle-input-two");
 
-    serial += 1;
-    const newValue = calcAreaThreeInput(0.5, circleInputOne, circleInputTwo);
-    const productName = pName('triangle');
+  serial += 1;
+  const newValue = calcAreaThreeInput(0.5, circleInputOne, circleInputTwo);
+  const productName = pName("triangle");
 
-    // display data
-    const getTable = document.getElementById('table');
-
-    // create element
-    const createNewElement = document.createElement('p');
-    createNewElement.innerHTML = showData(serial, productName, newValue);
-    getTable.appendChild(createNewElement);
-    
-
+  // display data
+  displayDataDinamic(serial, productName, newValue.toFixed(2));
 });
 
+document.getElementById("rectangle-btn").addEventListener("click", function () {
+  const circleInputOne = getInputValueOne("rectangle-input-one");
+  const circleInputTwo = getInputValueOne("rectangle-input-two");
 
-document.getElementById('rectangle-btn').addEventListener('click', function(){
-    const circleInputOne = getInputValueOne('rectangle-input-one');
-    const circleInputTwo = getInputValueOne('rectangle-input-two');
+  serial += 1;
+  const newValue = calcAreaTwoInput(circleInputOne, circleInputTwo);
+  const productName = pName("rectangle");
 
+  // display data
+  displayDataDinamic(serial, productName, newValue.toFixed(2));
+});
+
+document
+  .getElementById("parallelogram-btn")
+  .addEventListener("click", function () {
+    const circleInputOne = getInputValueOne("parallelogram-input-one");
+    const circleInputTwo = getInputValueOne("parallelogram-input-two");
+
+    serial += 1;
     const newValue = calcAreaTwoInput(circleInputOne, circleInputTwo);
+    const productName = pName("parallelogram");
+
+    // display data
+    displayDataDinamic(serial, productName, newValue.toFixed(2));
+  });
+
+document.getElementById("rhombus-btn").addEventListener("click", function () {
+  const circleInputOne = getInputValueOne("rhombus-input-one");
+  const circleInputTwo = getInputValueOne("rhombus-input-two");
+
+  serial += 1;
+  const newValue = calcAreaThreeInput(0.5, circleInputOne, circleInputTwo);
+  const productName = pName("rhombus");
+
+  // display data
+  displayDataDinamic(serial, productName, newValue.toFixed(2));
+});
+
+document.getElementById("pentagon-btn").addEventListener("click", function () {
+  const circleInputOne = getInputValueOne("pentagon-input-one");
+  const circleInputTwo = getInputValueOne("pentagon-input-two");
+
+  serial += 1;
+  const newValue = calcAreaThreeInput(0.5, circleInputOne, circleInputTwo);
+  const productName = pName("pentagon");
+
+  // display data
+  displayDataDinamic(serial, productName, newValue.toFixed(2));
+});
+
+document.getElementById("ellipse-btn").addEventListener("click", function () {
+  const circleInputOne = getInputValueOne("ellipse-input-one");
+  const circleInputTwo = getInputValueOne("ellipse-input-two");
+
+  serial += 1;
+  const newValue = calcAreaThreeInput(3.14, circleInputOne, circleInputTwo);
+  const productName = pName("ellipse");
+
+  // display data
+  displayDataDinamic(serial, productName, newValue.toFixed(2));
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,42 +1,52 @@
 // for 3 input calculation
-function calcAreaThreeInput(constantValue, firstInput, secondInput){
-    return constantValue * firstInput * secondInput;
-};
-
-// for 2 input calculation
-function calcAreaTwoInput(firstInput, secondInput){
-    return  firstInput * secondInput;
-};
-
-
-
-// get input value
-function getInputValueOne(id){
-    const InputOneField = document.getElementById(id);
-    const InputValueOneString = InputOneField.value;
-    const InputValueOne = parseFloat(InputValueOneString);
-
-    return InputValueOne;
+function calcAreaThreeInput(constantValue, firstInput, secondInput) {
+  return constantValue * firstInput * secondInput;
 }
 
-function getInputValueTwo(id){
-    const InputTwoField = document.getElementById(id);
-    const InputValueTwoString = InputTwoField.value;
-    const InputValueTwo = parseFloat(InputValueTwoString);
+// for 2 input calculation
+function calcAreaTwoInput(firstInput, secondInput) {
+  return firstInput * secondInput;
+}
 
-    return InputValueTwo;
+// get input value
+function getInputValueOne(id) {
+  const InputOneField = document.getElementById(id);
+  const InputValueOneString = InputOneField.value;
+  const InputValueOne = parseFloat(InputValueOneString);
+
+  return InputValueOne;
+}
+
+function getInputValueTwo(id) {
+  const InputTwoField = document.getElementById(id);
+  const InputValueTwoString = InputTwoField.value;
+  const InputValueTwo = parseFloat(InputValueTwoString);
+
+  return InputValueTwo;
 }
 
 // product name
-function pName(id){
-  const productName =  document.getElementById(id).innerText;
+function pName(id) {
+  const productName = document.getElementById(id).innerText;
   return productName;
 }
 
 // show date
-function showData(serial, productName, newValue){
-    const tableData = `
+function showData(serial, productName, newValue) {
+  const tableData = `
     <p>${serial}. ${productName}     ${newValue}</p>
-    `
-    return tableData;
+    `;
+  return tableData;
+}
+
+function displayDataDinamic(serial, productName, newValue) {
+  // display data
+  const getTable = document.getElementById("table");
+
+  // create element
+  const createNewElement = document.createElement("p");
+  createNewElement.innerHTML = showData(serial, productName, newValue);
+  getTable.appendChild(createNewElement);
+
+  return createNewElement;
 }
